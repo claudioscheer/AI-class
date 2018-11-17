5d0500b7a30381ecc8c7f43f4c78ae03a24519e2
Claudio Scheer <claudioscheer@protonmail.com>
2018-11-17 00:07:27 -0200

    New linear regression dataset

--- weka/Python/load-model.py
+++ weka/Python/load-model.py
@@ -2,8 +2,7 @@ from sklearn.externals import joblib
 
 regressor = joblib.load("./StockModelLinearRegression.pkl")
 y_pred = regressor.predict([
-    [2001, 0.6183, 0.6011, 0.6093, 1017479],  # 0.6183
-    [2011, 5.0143, 4.9801, 4.993, 1889690],  # 5.0018
+    [2020, 27.555, 27.832, 27.24, 168845],  # 27.652
 ])
 
 print(y_pred)
--- weka/join-stocks-csv.py
+++ weka/join-stocks-csv.py
@@ -5,24 +5,21 @@ import csv
 folder = "./Stocks"
 outputFile = csv.writer(open("Python/csv-data.csv", "w"))
 outputFile.writerow(
-    ["Company", "Date", "High", "Low", "Close", "Volume", "Open"])
+    ["Company", "Date", "Open", "High", "Low", "Volume", "Close"])
 
 for f in os.listdir(folder):
     print(f)
     file = csv.reader(open(os.path.join(folder, f)))
-    try:
-        headers = next(file)
-        for row in file:
-            outputFile.writerow([
-                f[:-4],
-                row[0],
-                row[2],
-                row[3],
-                row[4],
-                row[5],
-                row[1],
-            ])
-    except:
-        print("*****************")
+    headers = next(file)
+    for row in file:
+        outputFile.writerow([
+            f[:-4],
+            row[0],
+            row[1],
+            row[2],
+            row[3],
+            row[5],
+            row[4],
+        ])
 
 print("Done!")
